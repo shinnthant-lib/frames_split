e17d8429f0b95e7ed0ffd5f3df364b5784b75373
shinnthant-lib <shinnthant.lib@gmail.com>
2023-09-05 13:00:41 +0630

    Vic

---
+++ fill_frame.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+def fill_frame(frame):
+    h, w = frame.shape[0], frame.shape[1]
+    if h > w:
+        _pad = np.zeros([h, int((h - w) / 2), 3])
+        frame = np.concatenate([_pad, frame, _pad], axis=1)
+    elif h < w:
+        _pad = np.zeros([int((w - h) / 2), w, 3])
+        frame = np.concatenate([_pad, frame, _pad], axis=0)
+        frame = np.transpose(frame, [1, 0, 2])
+    frame = cv2.resize(frame, (1024, 1024))
+    return frame
--- frames_split.py
+++ frames_split.py
@@ -1,7 +1,11 @@
 import cv2 
 import os
+import numpy as np
 
-video_path=os.path.join("video","name.mp4")
+
+
+
+video_path=os.path.join("video","vic.mp4")
 frames_dir="frames"
 
 
@@ -17,13 +21,24 @@ frame_c = 1
 capframe=frame_len//nof
 countdown=1
 
-success,image = vidcap.read()
+success,frame = vidcap.read()
 while success:
     if countdown==capframe:
+        h, w = frame.shape[0], frame.shape[1]
+        if h > w:
+            _pad = np.zeros([h, int((h - w) / 2), 3])
+            frame = np.concatenate([_pad, frame, _pad], axis=1)
+        elif h < w:
+            _pad = np.zeros([int((w - h) / 2), w, 3])
+            frame = np.concatenate([_pad, frame, _pad], axis=0)
+            frame = np.transpose(frame, [1, 0, 2])
+        frame = cv2.resize(frame, (1024, 1024))
+        
         frame_path=os.path.join(frames_dir,f"{str(frame_c).zfill(0)}.jpg")
-        cv2.imwrite(frame_path,image)
+        cv2.imwrite(frame_path,frame)
         frame_c += 1
         countdown=0
     else:
         countdown+=1
-    success,image = vidcap.read()
+    success,frame = vidcap.read()
+
